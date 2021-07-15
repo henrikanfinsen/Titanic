c1783385bddb71b0193a569b1464d9762f077db7
Henrik A <henrik@deepinsight.io>
2021-07-15 12:55:42 +0200

    Resolved

--- main.py
+++ main.py
@@ -4,14 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier
-<<<<<<< HEAD
 from sklearn.metrics import confusion_matrix, precision_score
 =======
-<<<<<<< HEAD
-from sklearn.metrics import confusion_matrix, precision_score
-=======
->>>>>>> 5f46d9a4a88307c1186969a0f0672f32dc964ee5
->>>>>>> aeca4ddb51ae0dcebc9a3f576a8f51b9a24455ad
 from sklearn.svm import LinearSVC
 
 # %% Read data
@@ -35,9 +29,6 @@ for data in all_data:
     data['Status'] = data['Name'].str.extract(' ([A-Za-z]+)\.', expand=False)
 <<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-
 # %%
 Status_mapping = {"Mr": 0, "Miss": 1, "Mrs": 2,
                   "Master": 3, "Dr": 3, "Rev": 3, "Col": 3, "Major": 3, "Mlle": 3, "Countess": 3,
@@ -60,46 +51,6 @@ test['Fare'].fillna(train['Fare'].mean(), inplace=True)
 train['FamilySize'] = train['SibSp'] + train['Parch'] + 1
 test['FamilySize'] = test['SibSp'] + test['Parch'] + 1
 
-# %%
-y = train["Survived"]
-
-features = ["Pclass", "Age", "Sex", "FamilySize", "Status", "Fare"]
-X = pd.get_dummies(train[features])
-X_test = pd.get_dummies(test[features])
-
-model = RandomForestClassifier(n_estimators=100, max_depth=3, random_state=2)
-# model = LinearSVC()
-model.fit(X, y)
-predictions = model.predict(X_test)
-
-predictions2 = model.predict(X)
-print(confusion_matrix(y, predictions2))
-print(precision_score(y, predictions2))
-print(1 - sum(np.abs(y - predictions2)) / len(y))
-=======
->>>>>>> aeca4ddb51ae0dcebc9a3f576a8f51b9a24455ad
-
-# %%
-Status_mapping = {"Mr": 0, "Miss": 1, "Mrs": 2,
-                  "Master": 3, "Dr": 3, "Rev": 3, "Col": 3, "Major": 3, "Mlle": 3, "Countess": 3,
-                  "Ms": 3, "Lady": 3, "Jonkheer": 3, "Don": 3, "Dona": 3, "Mme": 3, "Capt": 3, "Sir": 3}
-for dataset in all_data:
-    dataset['Status'] = dataset['Status'].map(Status_mapping)
-
-train['Age'].fillna(train.groupby("Status")["Age"].transform("median"), inplace=True)
-test['Age'].fillna(test.groupby("Status")["Age"].transform("median"), inplace=True)
-
-train = train.drop('Name', axis=1)
-test = test.drop('Name', axis=1)
-
-
-# %% Interpolate the missing "Fare"
-train['Fare'].fillna(train['Fare'].mean(), inplace=True)
-test['Fare'].fillna(train['Fare'].mean(), inplace=True)
-
-# %% Family size
-train['FamilySize'] = train['SibSp'] + train['Parch'] + 1
-test['FamilySize'] = test['SibSp'] + test['Parch'] + 1
 
 # %% New feature: 'Minor'
 train['Minor'] = train['Age'] < 16
@@ -117,23 +68,11 @@ model = RandomForestClassifier(n_estimators=120, max_depth=3, random_state=3)
 model.fit(X, y)
 predictions = model.predict(X_test)
 
-<<<<<<< HEAD
 predictions2 = model.predict(X)
 print(confusion_matrix(y, predictions2))
 print(precision_score(y, predictions2))
 print(1 - sum(np.abs(y - predictions2)) / len(y))
-=======
 
 # %%
 output = pd.DataFrame({'PassengerId': test.PassengerId, 'Survived': predictions})
 output.to_csv('my_submission.csv', index=False)
-
->>>>>>> 5f46d9a4a88307c1186969a0f0672f32dc964ee5
-
-
-
-# %%
-output = pd.DataFrame({'PassengerId': test.PassengerId, 'Survived': predictions})
-output.to_csv('my_submission.csv', index=False)
->>>>>>> aeca4ddb51ae0dcebc9a3f576a8f51b9a24455ad
-
